46498d3df14ea268066127b0ac8d822912ac4a4a
John Kuefler <john.kuefler@hotmail.com>
2018-08-06 09:18:33 -0500

    vanilla example ready

--- Payroll.Tests/PayrollCalculatorTests.cs
+++ Payroll.Tests/PayrollCalculatorTests.cs
@@ -2,7 +2,7 @@ using System;
 using Moq;
 using Payroll.Core;
 using Payroll.Services;
-using Payroll.Tests.Builders.Object;
+using Payroll.Services.POCO;
 using Xunit;
 
 namespace Payroll.Tests
@@ -13,22 +13,36 @@ namespace Payroll.Tests
         public void CalculatePayroll_ZeroHours_ShouldReturnZero()
         {
             // arrange
+            TaxBracket taxBracket = new TaxBracket
+            {
+                TaxRate = 0.2
+            };
+
+            Employee employee = new Employee
+            {
+                Seniority = false,
+                HourlyRate = 25
+            };
+
+            TimeCard timeCard = new TimeCard
+            {
+                TotalHours = 0
+            };
+
             Mock<ITaxService> taxServiceMock = new Mock<ITaxService>();
             Mock<IEmployeeService> employeeServiceMock = new Mock<IEmployeeService>();
             Mock<ITimeCardService> timeCardServiceMock = new Mock<ITimeCardService>();
 
-            taxServiceMock.Setup(x => x.GetTaxBracket(It.IsAny<double>())).Returns(new TaxBracketTestBuilder().Build());
-
-            employeeServiceMock.Setup(x => x.GetById(It.IsAny<string>())).Returns(new EmployeeTestBuilder().Build());
-
+            taxServiceMock.Setup(x => x.GetTaxBracket(It.IsAny<double>())).Returns(taxBracket);
+            employeeServiceMock.Setup(x => x.GetById(It.IsAny<string>())).Returns(employee);
             timeCardServiceMock
                 .Setup(x => x.GetEmployeeTimeCard(It.IsAny<string>(), It.IsAny<DateTime>(), It.IsAny<DateTime>()))
-                .Returns(new TimeCardTestBuilder().WithTotalHours(0).Build());
+                .Returns(timeCard);
 
-            // act
             var sut = new PayrollCalculator(timeCardServiceMock.Object, employeeServiceMock.Object,
                 taxServiceMock.Object);
 
+            // act
             double payroll = sut.CalculatePayroll(DateTime.Now, DateTime.Now.AddDays(7), "1");
 
             // assert
@@ -41,32 +55,85 @@ namespace Payroll.Tests
         [InlineData(0.15, 7.25, 20, false, 123.25)]
         [InlineData(0.30, 60, 50, true, 2170)]
         [InlineData(0, 10, 40, true, 500)]
-        public void CalculatePayroll_ValidInputData_ShouldProduceCorrectPayroll(double taxRate, 
-            double hourlyRate, int hours, bool seniority, double payroll)
+        public void CalculatePayroll_ValidInputData_ShouldProduceCorrectPayroll(double taxRate,
+            double hourlyRate, int timeCardHours, bool seniority, double payroll)
         {
             // arrange
+            TaxBracket taxBracket = new TaxBracket
+            {
+                TaxRate = taxRate
+            };
+
+            Employee employee = new Employee
+            {
+                Seniority = seniority,
+                HourlyRate = hourlyRate
+            };
+
+            TimeCard timeCard = new TimeCard
+            {
+                TotalHours = timeCardHours
+            };
+
             Mock<ITaxService> taxServiceMock = new Mock<ITaxService>();
             Mock<IEmployeeService> employeeServiceMock = new Mock<IEmployeeService>();
             Mock<ITimeCardService> timeCardServiceMock = new Mock<ITimeCardService>();
 
-            taxServiceMock.Setup(x => x.GetTaxBracket(It.IsAny<double>()))
-                .Returns(new TaxBracketTestBuilder().WithTaxRate(taxRate).Build());
-
-            employeeServiceMock.Setup(x => x.GetById(It.IsAny<string>()))
-                .Returns(new EmployeeTestBuilder().WithHourlyRate(hourlyRate).WithSeniority(seniority).Build());
-
+            taxServiceMock.Setup(x => x.GetTaxBracket(It.IsAny<double>())).Returns(taxBracket);
+            employeeServiceMock.Setup(x => x.GetById(It.IsAny<string>())).Returns(employee);
             timeCardServiceMock
                 .Setup(x => x.GetEmployeeTimeCard(It.IsAny<string>(), It.IsAny<DateTime>(), It.IsAny<DateTime>()))
-                .Returns(new TimeCardTestBuilder().WithTotalHours(hours).Build());
+                .Returns(timeCard);
 
-            // act
             var sut = new PayrollCalculator(timeCardServiceMock.Object, employeeServiceMock.Object,
                 taxServiceMock.Object);
 
+            // act
             double amount = sut.CalculatePayroll(DateTime.Now, DateTime.Now.AddDays(7), "1");
 
             // assert
             Assert.Equal(payroll, amount);
+        }
+
+
+        [Fact]
+        public void CalculatePayroll_NegativeHours_ShouldReturnZero()
+        {
+            // arrange
+            TaxBracket taxBracket = new TaxBracket
+            {
+                TaxRate = 0.2
+            };
+
+            Employee employee = new Employee
+            {
+                Seniority = false,
+                HourlyRate = 25
+            };
+
+            TimeCard timeCard = new TimeCard
+            {
+                TotalHours = -1
+            };
+
+            Mock<ITaxService> taxServiceMock = new Mock<ITaxService>();
+            Mock<IEmployeeService> employeeServiceMock = new Mock<IEmployeeService>();
+            Mock<ITimeCardService> timeCardServiceMock = new Mock<ITimeCardService>();
+
+            taxServiceMock.Setup(x => x.GetTaxBracket(It.IsAny<double>())).Returns(taxBracket);
+            employeeServiceMock.Setup(x => x.GetById(It.IsAny<string>())).Returns(employee);
+            timeCardServiceMock
+                .Setup(x => x.GetEmployeeTimeCard(It.IsAny<string>(), It.IsAny<DateTime>(), It.IsAny<DateTime>()))
+                .Returns(timeCard);
+
+            var sut = new PayrollCalculator(timeCardServiceMock.Object, employeeServiceMock.Object,
+                taxServiceMock.Object);
+
+            // act
+            double amount = sut.CalculatePayroll(DateTime.Now, DateTime.Now.AddDays(7), "1");
+
+            // assert
+            Assert.Equal(0, amount);
         }
     }
 }
